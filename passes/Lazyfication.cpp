935440ae55edc8e34167ce29730522bff8c21d43
brenocfg <brenocfg@gmail.com>
2022-04-01 12:47:15 -0300

    Fix a few bugs in Lazyfication. Schedule it as LTO pass instead.

--- passes/Lazyfication.cpp
+++ passes/Lazyfication.cpp
@@ -1,3 +1,4 @@
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/Analysis/LoopInfo.h"
@@ -12,6 +13,7 @@
 #include "Lazyfication.h"
 #include "ProgramSlice.h"
 
+#include <fstream>
 #include <random>
 
 #define DEBUG_TYPE "WyvernLazyficationPass"
@@ -29,12 +31,37 @@ STATISTIC(TotalSliceSize,
 
 using namespace llvm;
 
-static cl::opt<bool> WyvernLazificationMemoization(
+static cl::opt<bool> WyvernLazyficationMemoization(
     "wylazy-memo", cl::init(true),
     cl::desc(
         "Wyvern - Enable memoization in Lazyfication (implement call-by-need"
         "rather than call-by-name)."));
 
+static cl::opt<bool> WyvernEnablePGO(
+    "wylazy-pgo", cl::init(false),
+    cl::desc("Wyvern - Enable Profile-Guided Optimization. Requires an "
+             "instrumentation-generated profile report for the program."));
+
+static cl::opt<std::string>
+    WyvernPGOFilePath("wylazy-pgo-file", cl::init(""),
+                      cl::desc("Wyvern - Path to instrumentation output file "
+                               "containing profile information for PGO."));
+
+static cl::opt<double> WyvernPGOThreshold(
+    "wylazy-pgo-threshold", cl::init(0.4),
+    cl::desc("Wyvern - Argument evaluation percentage threshold below which "
+             "callsite should be lazyfied."));
+
+static cl::opt<bool> WyvernLazyfication(
+    "wylazy-enable", cl::init(true),
+    cl::desc("Wyvern - Controls whether to enable lazyfication at all (used "
+             "for comparison against O3 baseline)"));
+
+/**
+ * Returns number of instructions in Function @param F. Is used to compute the
+ * size of delegate functions generated through slicing.
+ *
+ */
 static unsigned int getNumberOfInsts(Function &F) {
   unsigned int size = 0;
   for (BasicBlock &BB : F) {
@@ -45,6 +72,96 @@ static unsigned int getNumberOfInsts(Function &F) {
   return size;
 }
 
+bool WyvernLazyficationPass::shouldLazifyCallsitePGO(CallInst *CI,
+                                                     uint8_t argIdx) {
+  WyvernCallSiteProfInfo *prof_info = profileInfo[CI].get();
+
+  if (!prof_info) {
+    return false;
+  }
+
+  uint64_t numCalls = prof_info->_numCalls;
+  uint64_t uniqueEvals = prof_info->_uniqueEvals[argIdx];
+  double evalRate = (double)uniqueEvals / (double)numCalls;
+
+  if (evalRate < WyvernPGOThreshold) {
+    return true;
+  }
+
+  return false;
+}
+
+bool WyvernLazyficationPass::loadProfileInfo(Module &M, std::string path) {
+  std::string line;
+  std::ifstream profileReportFile(path);
+  if (!profileReportFile.is_open()) {
+    return false;
+  }
+
+  std::string callerName;
+  uint64_t instID, numCalls, numArgs;
+  std::string argEvals;
+
+  profileReportFile.ignore(std::numeric_limits<std::streamsize>::max(),
+                           profileReportFile.widen('\n'));
+
+  std::map<std::string,
+           std::map<uint64_t, std::unique_ptr<WyvernCallSiteProfInfo>>>
+      rawProfInfo;
+  std::string parsed_val;
+  while (getline(profileReportFile, parsed_val, ',')) {
+    callerName = parsed_val;
+    getline(profileReportFile, parsed_val, ',');
+    instID = stol(parsed_val);
+    getline(profileReportFile, parsed_val, ',');
+    numCalls = stol(parsed_val);
+    getline(profileReportFile, parsed_val, ',');
+    numArgs = stol(parsed_val);
+
+    auto newEntry = std::make_unique<WyvernCallSiteProfInfo>(numArgs, numCalls);
+
+    for (int i = 0; i < numArgs; ++i) {
+      getline(profileReportFile, parsed_val, ',');
+      newEntry->_uniqueEvals[i] = stol(parsed_val);
+    }
+    for (int i = 0; i < numArgs; ++i) {
+      getline(profileReportFile, parsed_val, ',');
+      newEntry->_totalEvals[i] = stol(parsed_val);
+    }
+    profileReportFile.ignore(std::numeric_limits<std::streamsize>::max(),
+                             profileReportFile.widen('\n'));
+
+    if (M.getFunction(callerName) == nullptr) {
+      continue;
+    }
+
+    rawProfInfo[callerName][instID] = std::move(newEntry);
+  }
+
+  for (auto &[funName, prof_infos] : rawProfInfo) {
+    Function *F = M.getFunction(funName);
+    inst_iterator I = inst_begin(F);
+    unsigned inst_id = 0;
+    for (; I != inst_end(F); ++I, ++inst_id) {
+      if (CallBase *CB = dyn_cast<CallBase>(&*I)) {
+        if (prof_infos.count(inst_id)) {
+          profileInfo[CB] = std::move(prof_infos[inst_id]);
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
+/**
+ * When lazifying a callsite, there may be real arguments which originally had
+ * associated parameters that change their optimization/implementation
+ * semantics, for instance, noalias or byref/byval. Since we replace these
+ * arguments by a thunk, these attributes are no longer valid. This function
+ * removes them.
+ *
+ */
 void removeAttributesFromThunkArgument(Value &V, unsigned int index) {
   AttributeMask toRemove;
 
@@ -52,7 +169,6 @@ void removeAttributesFromThunkArgument(Value &V, unsigned int index) {
   toRemove.addAttribute(Attribute::ZExt);
   toRemove.addAttribute(Attribute::NoAlias);
   toRemove.addAttribute(Attribute::ByRef);
-  toRemove.addAttribute(Attribute::NoAlias);
   toRemove.addAttribute(Attribute::NoCapture);
   toRemove.addAttribute(Attribute::ByVal);
   toRemove.addAttribute(Attribute::ReadOnly);
@@ -65,6 +181,14 @@ void removeAttributesFromThunkArgument(Value &V, unsigned int index) {
   }
 }
 
+/**
+ * At this point, Function @param F was lazyfied in terms of Argument
+ * @param optimizedArg, so that the formal parameter is now a thunk. However,
+ * uses of @param optimizedArg still use it as a value. This function replaces
+ * all uses of @param optimizedArg by an invocation of the function pointer
+ * contained within the thunk.
+ *
+ */
 void updateThunkArgUses(Function *F, Argument *optimizedArg,
                         Function *slicedFunction) {
   std::map<User *, CallInst *> userCalls;
@@ -75,7 +199,7 @@ void updateThunkArgUses(Function *F, Argument *optimizedArg,
   for (auto &Use : optimizedArg->uses()) {
     Instruction *UserI = dyn_cast<Instruction>(Use.getUser());
     if (UserI && !userCalls.count(UserI)) {
-      // if the use is a PHINode, the use happens at the edge, so we cannot
+      // If the use is a PHINode, the use happens at the edge, so we cannot
       // insert the thunk load/call at the PHI's block. Instead, we must insert
       // them at the end of the block which flows into the PHI node
       if (PHINode *PN = dyn_cast<PHINode>(UserI)) {
@@ -84,6 +208,7 @@ void updateThunkArgUses(Function *F, Argument *optimizedArg,
       } else {
         builder.SetInsertPoint(UserI);
       }
+
       Value *thunkFPtrGEP = builder.CreateStructGEP(
           optimizedArg->getType()->getPointerElementType(), optimizedArg, 0,
           "_wyvern_thunk_fptr_addr");
@@ -95,22 +220,33 @@ void updateThunkArgUses(Function *F, Argument *optimizedArg,
       CallInst *thunkCall =
           builder.CreateCall(slicedFunction->getFunctionType(), thunkFPtrLoad,
                              {optimizedArg}, "_wyvern_thunkcall");
+
+      // Replacing uses/users immediately can break use-def chains. Instead,
+      // keep track of all users to be updated.
       userCalls[UserI] = thunkCall;
     }
   }
 
   for (auto &Use : optimizedArg->uses()) {
     Instruction *UserI = dyn_cast<Instruction>(Use.getUser());
+    // Replacing uses/users immediately can break use-def chains. Instead, keep
+    // track of all uses to be updated.
     if (UserI && userCalls.count(UserI)) {
       usesToChange.insert(&Use);
     }
   }
 
+  // Update uses
   for (auto *Use : usesToChange) {
     Use->set(userCalls[Use->getUser()]);
   }
 }
 
+/**
+ * Clones function @param Callee, replacing its formal parameter of index
+ * @param index with thunk @param thunkArg.
+ *
+ */
 Function *cloneCalleeFunction(Function &Callee, int index,
                               Function &slicedFunction, Value *thunkArg,
                               Module &M) {
@@ -153,12 +289,12 @@ Function *cloneCalleeFunction(Function &Callee, int index,
   return newCallee;
 }
 
-bool WyvernLazyficationPass::lazifyCallsite(CallInst &CI, int index,
+bool WyvernLazyficationPass::lazifyCallsite(CallInst &CI, uint8_t index,
                                             Module &M) {
   Instruction *lazyfiableArg;
 
   if (!(lazyfiableArg = dyn_cast<Instruction>(CI.getArgOperand(index)))) {
-    errs() << "Argument is not lazyfiable!\n";
+    LLVM_DEBUG(dbgs() << "Argument is not lazyfiable!\n");
     return false;
   }
 
@@ -175,6 +311,12 @@ bool WyvernLazyficationPass::lazifyCallsite(CallInst &CI, int index,
   }
 
   Function *callee = CI.getCalledFunction();
+  if (!callee || callee->isDeclaration()) {
+    LLVM_DEBUG(dbgs() << "Cannot lazify argument. Callee function definition "
+                         "is not available for cloning!\n");
+    return false;
+  }
+
   LLVM_DEBUG(dbgs() << "Lazifying: " << *lazyfiableArg << " in func "
                     << caller->getName() << " call to " << callee->getName()
                     << "\n");
@@ -185,12 +327,13 @@ bool WyvernLazyficationPass::lazifyCallsite(CallInst &CI, int index,
   Function *thunkFunction, *newCallee;
   AllocaInst *thunkAlloca;
   StructType *thunkStructType;
-  if (WyvernLazificationMemoization) {
+  if (WyvernLazyficationMemoization) {
     std::tie(thunkFunction, thunkStructType) = slice.memoizedOutline();
 
     // allocate thunk, initialize it with:
     // struct thunk {
     //   fptr = thunkFunction
+    //   memoed_value = undef
     //   memo_flag = 0
     //   arg0 = x
     //   arg1 = y
@@ -207,7 +350,7 @@ bool WyvernLazyficationPass::lazifyCallsite(CallInst &CI, int index,
                                 thunkAlloca, 2, "_wyvern_thunk_flag_gep");
     builder.CreateStore(builder.getInt1(0), thunkFlagGEP);
 
-    unsigned int i = 3;
+    uint64_t i = 3;
     for (auto &arg : slice.getOrigFunctionArgs()) {
       Value *thunkArgGEP = builder.CreateStructGEP(
           thunkAlloca->getType()->getPointerElementType(), thunkAlloca, i,
@@ -232,7 +375,7 @@ bool WyvernLazyficationPass::lazifyCallsite(CallInst &CI, int index,
                                 thunkAlloca, 0, "_wyvern_thunk_fptr_gep");
     builder.CreateStore(thunkFunction, thunkFPtrGEP);
 
-    unsigned int i = 1;
+    uint64_t i = 1;
     for (auto &arg : slice.getOrigFunctionArgs()) {
       Value *thunkArgGEP = builder.CreateStructGEP(
           thunkAlloca->getType()->getPointerElementType(), thunkAlloca, i,
@@ -249,7 +392,7 @@ bool WyvernLazyficationPass::lazifyCallsite(CallInst &CI, int index,
   removeAttributesFromThunkArgument(CI, index);
   removeAttributesFromThunkArgument(*newCallee, index);
 
-  unsigned int sliceSize = getNumberOfInsts(*thunkFunction);
+  uint64_t sliceSize = getNumberOfInsts(*thunkFunction);
   TotalSliceSize += sliceSize;
   if (LargestSliceSize < sliceSize) {
     LargestSliceSize = sliceSize;
@@ -264,14 +407,41 @@ bool WyvernLazyficationPass::lazifyCallsite(CallInst &CI, int index,
 bool WyvernLazyficationPass::runOnModule(Module &M) {
   SmallestSliceSize = std::numeric_limits<unsigned int>::max();
   FindLazyfiableAnalysis &FLA = getAnalysis<FindLazyfiableAnalysis>();
+  
+  if (!WyvernLazyfication) {
+    return false;
+  }
 
   bool changed = false;
-  for (auto &pair : FLA.getLazyfiableCallSites()) {
-    CallInst *callInst = pair.first;
-    int argIdx = pair.second;
-    Function *callee = pair.first->getCalledFunction();
-    if (FLA.getLazyfiablePaths().count(std::make_pair(callee, argIdx)) > 0) {
-      changed = lazifyCallsite(*callInst, argIdx, M);
+  if (WyvernEnablePGO) {
+    if (!loadProfileInfo(M, WyvernPGOFilePath)) {
+      errs() << "Failed to load profile info for PGO! Exiting...\n";
+      return false;
+    }
+
+    for (Function &F : M) {
+      for (inst_iterator I = inst_begin(F); I != inst_end(F); ++I) {
+        if (!isa<CallInst>(&*I)) {
+          continue;
+        }
+        CallInst *CI = cast<CallInst>(&*I);
+        for (uint8_t argIdx = 0; argIdx < CI->arg_size(); ++argIdx) {
+          if (shouldLazifyCallsitePGO(CI, argIdx)) {
+            changed = lazifyCallsite(*CI, argIdx, M);
+          }
+        }
+      }
+    }
+  }
+
+  else {
+    for (auto &pair : FLA.getLazyfiableCallSites()) {
+      CallInst *CI = pair.first;
+      uint8_t argIdx = pair.second;
+      Function *callee = pair.first->getCalledFunction();
+      if (FLA.getLazyfiablePaths().count(std::make_pair(callee, argIdx)) > 0) {
+        changed = lazifyCallsite(*CI, argIdx, M);
+      }
     }
   }
 
@@ -290,7 +460,7 @@ void WyvernLazyficationPass::getAnalysisUsage(AnalysisUsage &AU) const {
 }
 
 static llvm::RegisterStandardPasses RegisterWyvernLazification(
-    llvm::PassManagerBuilder::EP_ModuleOptimizerEarly,
+    llvm::PassManagerBuilder::EP_FullLinkTimeOptimizationEarly,
     [](const llvm::PassManagerBuilder &Builder,
        llvm::legacy::PassManagerBase &PM) {
       PM.add(new WyvernLazyficationPass());
--- passes/Lazyfication.h
+++ passes/Lazyfication.h
@@ -1,11 +1,43 @@
+#include "llvm/ADT/SmallVector.h"
+
+#include <set>
+#include <unordered_map>
+#include <utility>
+
 namespace llvm {
+struct WyvernCallSiteProfInfo {
+  WyvernCallSiteProfInfo(uint8_t numArgs, uint64_t numCalls) {
+    _uniqueEvals = SmallVector<int64_t>(numArgs);
+    _totalEvals = SmallVector<int64_t>(numArgs);
+    _numCalls = numCalls;
+  }
+
+  uint64_t _numCalls;
+  SmallVector<int64_t> _uniqueEvals;
+  SmallVector<int64_t> _totalEvals;
+};
+
 struct WyvernLazyficationPass : public ModulePass {
   static char ID;
   WyvernLazyficationPass() : ModulePass(ID) {}
-  bool lazifyCallsite(CallInst &CI, int index, Module &M);
+
+  /**
+   * Lazifies the function call @param CI in terms of its actual parameter of
+   * index @param index. To do so, the instructions involved in computing the
+   * parameter of index @param index are encapsulated in a delegate function
+   * generated through program slicing.
+   *
+   */
+  bool lazifyCallsite(CallInst &CI, uint8_t index, Module &M);
+
+  bool shouldLazifyCallsitePGO(CallInst *CI, uint8_t argIdx);
+
+  bool loadProfileInfo(Module &M, std::string path);
+  std::set<std::pair<Function *, Instruction *>> lazifiedFunctions;
+  std::unordered_map<CallBase *, std::unique_ptr<WyvernCallSiteProfInfo>>
+      profileInfo;
 
   bool runOnModule(Module &);
   void getAnalysisUsage(AnalysisUsage &) const;
-  std::set<std::pair<Function *, Instruction *>> lazifiedFunctions;
 };
 } // namespace llvm
