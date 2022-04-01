7616c36c434b889329c1a8595f195cfe00d1dab9
brenocfg <brenocfg@gmail.com>
2022-04-01 12:47:45 -0300

    Change test script to run pass as LTO pass.

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -22,27 +22,11 @@ echo "COMMAND: $COMMAND"
 for f in ${TEST_FILES}; do
 	echo ========= Running test ${f} =========
 	if [ "$USE_CLANG" = "true" ]; then
-		COMMAND=$(cat <<-END
-		clang -flegacy-pass-manager -Xclang -load -Xclang ../build/passes/libWyvern.so ${f} -O3 -mllvm -stats -o test
-		END)
+		clang -flegacy-pass-manager -flto -Xclang -disable-O0-optnone -fuse-ld=/home/brenocfg/Work/llvm-trunk/build/bin/ld.lld -Wl,-mllvm=-load=/home/brenocfg/Work/wyvern/build/passes/libWyvern.so  ${f} -O0 -Wl,-mllvm=-stats -o test;
 	else
-		COMMAND=$(cat <<-END
-			clang -S -c -emit-llvm -Xclang -disable-O0-optnone ${f} -o test.ll;
-			opt -load ~/Work/wyvern/build/passes/libWyvern.so -S
-			-mem2reg `#promote to registers`
-			-mergereturn  `#merge all returns`
-			-function-attrs  `#add fn attributes`
-			-loop-simplify  `#simplifies loops to more canonical forms`
-			-lcssa `#gates loops with phi functions for variable uses`
-			-enable-new-pm=0 `#disables new passManager`
-			-lazify-callsites  `#calls the Wyvern optimization pass`
-			-wylazy-memo=${MEMO_FLAG} `#argument do determine to use memoization`
-			-stats `#print llvm pass stats`
-			test.ll `#input file`
-			-o test_lazyfied.ll `#output file`
-			END)
+		clang -S -c -emit-llvm -Xclang -disable-O0-optnone ${f} -o test.ll;
+		opt -load ~/Work/wyvern/build/passes/libWyvern.so -S -mem2reg -mergereturn -function-attrs -loop-simplify -lcssa -enable-new-pm=0 -lazify-callsites -wylazy-memo=${MEMO_FLAG} -stats test.ll -o test_lazyfied.ll;
 	fi
-	$COMMAND
 	echo ========= Done with ${f}! =========
 	if [[ "$RUN" = "false" ]]; then
 		echo "Not running test!"
