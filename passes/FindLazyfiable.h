be3ef455863eefe49c161408b103cf9176e3820e
Fernando Quintao Pereira <pronesto@gmail.com>
2022-05-30 14:58:16 -0300

    Adding comments.

--- passes/FindLazyfiable.h
+++ passes/FindLazyfiable.h
@@ -1,3 +1,19 @@
+//===- FindLazyfiable.h - Finds call sites candidates for lazification`----===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+/// \file
+/// TODO: check this comment:
+/// This file provides the interface for the analysis that finds calls sites
+/// that are good candidates to be lazified. A good candidate for lazification
+/// is a call site that calls a "promising" function. A function is deemed
+/// promising if it contains a path from its entry node until an exit point
+/// that does not crosses any use of at least one of its formal parameters.
+///
+//===----------------------------------------------------------------------===//
 #include <map>
 #include <set>
 #include <stack>
@@ -20,25 +36,40 @@ public:
   bool runOnModule(Module &);
   void getAnalysisUsage(AnalysisUsage &) const;
 
+  /// TODO: explain what the function does. In particular, why does it return
+  /// a set of functions, but is called "stats"?
   const std::set<Function *> &getLazyFunctionStats() {
     return _lazyFunctionsStats;
   }
+
+  /// TODO: explain what the function does. Explain why the return value is
+  /// a set of pairs.
   const std::set<std::pair<Function *, int>> &getLazyPathsStats() {
     return _lazyPathsStats;
   }
+
+  /// TODO: explain what this function does. In particular, why the return is
+  /// like this: a map of pairs to integers? Your return types are definitely
+  /// a bit convoluted and hard to grok.
   const std::map<std::pair<Function *, int>, int> &
   getLazyfiableCallSiteStats() {
     return _lazyfiableCallSitesStats;
   }
 
+  /// TODO: explain what this function does.
   const std::set<std::pair<Function *, int>> &getLazyfiablePaths() {
     return _lazyfiablePaths;
   }
+
+  /// TODO: explain what this function does.
   const std::set<std::pair<CallInst *, int>> &getLazyfiableCallSites() {
     return _lazyfiableCallSites;
   }
 
 private:
+  /// TODO: I think we can understand quite a lot of what this pass does if
+  /// we can understand how you use the data structures. So, please, explain
+  /// them!
   std::set<Function *> _lazyFunctionsStats;
   std::set<std::pair<Function *, int>> _lazyPathsStats;
   std::map<std::pair<Function *, int>, int> _lazyfiableCallSitesStats;
