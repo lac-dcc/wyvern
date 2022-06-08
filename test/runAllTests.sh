e9d47669c59ee973ffc47cb870a032222fb24131
brenocfg <brenocfg@gmail.com>
2022-06-08 14:06:16 -0300

    Make lib path relative to repo base path in test script.

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -16,21 +16,12 @@ fi
 echo "MEMO_FLAG=${MEMO_FLAG}"
 echo "CLANG=${USE_CLANG}"
 
-
-echo "COMMAND: $COMMAND"
-
 for f in ${TEST_FILES}; do
-	echo ========= Running test ${f} =========
+	echo "========= Running test ${f} ========="
 	if [ "$USE_CLANG" = "true" ]; then
-		clang -flegacy-pass-manager -flto -Xclang -disable-O0-optnone -fuse-ld=/home/brenocfg/Work/llvm-trunk/build/bin/ld.lld -Wl,-mllvm=-load=/home/brenocfg/Work/wyvern/build/passes/libWyvern.so  ${f} -O0 -Wl,-mllvm=-stats -o test;
+		clang -flegacy-pass-manager -flto -Xclang -disable-O0-optnone -fuse-ld=lld -Wl,-mllvm=-load=../build/passes/libWyvern.so ${f} -O0 -Wl,-mllvm=-stats -o test
 	else
-		clang -S -c -emit-llvm -Xclang -disable-O0-optnone ${f} -o test.ll;
-		opt -load ~/Work/wyvern/build/passes/libWyvern.so -S -mem2reg -mergereturn -function-attrs -loop-simplify -lcssa -enable-new-pm=0 -lazify-callsites -wylazy-memo=${MEMO_FLAG} -stats test.ll -o test_lazyfied.ll;
-	fi
-	echo ========= Done with ${f}! =========
-	if [[ "$RUN" = "false" ]]; then
-		echo "Not running test!"
-		continue
+		clang -S -c -emit-llvm -Xclang -disable-O0-optnone ${f} -o test.ll
+		opt -load ../build/passes/libWyvern.so -S -mem2reg -mergereturn -function-attrs -loop-simplify -lcssa -enable-new-pm=0 -lazify-callsites -wylazy-memo=${MEMO_FLAG} -instcombine -stats test.ll -o test_lazyfied.ll
 	fi
 done
-
